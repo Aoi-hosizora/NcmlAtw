0d7960f06507d72f95fc6da95ce48d7d1fd007d1
AoiHosizora <a970335605@hotmail.com>
2019-09-14 18:32:36 +0800

    Use RemoteViews & RemoteController

--- NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MainActivity.java
+++ NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MainActivity.java
@@ -1,12 +1,15 @@
 package com.aoihosizora.neteasem2dserver;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,28 +21,46 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     public static String TAG = "MainActivity";
-    public static int REQUEST_PERMISSION_CODE = 1;
+
+    private Intent ServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {
-                    Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE
-            }, REQUEST_PERMISSION_CODE);
+        ButterKnife.bind(this);
+        ServiceIntent = new Intent(this, MusicNotificationService.class);
+
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
+        if (!isNotificationListenersEnabled()) {
+            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this, "Grant permission failed", Toast.LENGTH_LONG).show();
+    /**
+     * 通知へのアクセス
+     * @return
+     */
+    public boolean isNotificationListenersEnabled() {
+        String pkgName = getPackageName();
+        final String flat = Settings.Secure.getString(getContentResolver(), "enabled_notification_listeners");
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (String name : names) {
+                final ComponentName cn = ComponentName.unflattenFromString(name);
+                if (cn != null && TextUtils.equals(pkgName, cn.getPackageName()))
+                    return true;
+            }
         }
+        return false;
     }
 
     @BindView(R.id.id_btn_server)
@@ -47,13 +68,22 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.id_btn_server)
     void on_btn_server_clicked(View view) {
-        Intent intent = new Intent(this, MusicNotificationService.class);
         if (m_btn_server.getText().equals(getString(R.string.btn_start))) {
             m_btn_server.setText(getString(R.string.btn_stop));
-            startService(intent);
+            try {
+                stopService(ServiceIntent);
+            }
+            catch (Exception ex) { }
+            startService(ServiceIntent);
         } else {
             m_btn_server.setText(getString(R.string.btn_start));
-            stopService(intent);
+            stopService(ServiceIntent);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(ServiceIntent);
+        super.onDestroy();
     }
 }
--- NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MusicNotificationService.java
+++ NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MusicNotificationService.java
@@ -1,20 +1,94 @@
 package com.aoihosizora.neteasem2dserver;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.RemoteController;
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RemoteViews;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MusicNotificationService extends NotificationListenerService {
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class MusicNotificationService extends NotificationListenerService implements RemoteController.OnClientUpdateListener {
 
     public static String TAG = "MusicNotificationService";
 
     @Override
-    public void onNotificationPosted(StatusBarNotification sbn) {
-        Toast.makeText(this, "onNotificationPosted", Toast.LENGTH_SHORT).show();
-        // Toast.makeText(this, sbn.getPackageName(), Toast.LENGTH_SHORT).show();
-        // Toast.makeText(this, sbn.getNotification().toString(), Toast.LENGTH_SHORT).show();
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            RemoteController remoteController = new RemoteController(this, this);
+            ((AudioManager) getSystemService(AUDIO_SERVICE)).registerRemoteController(remoteController);
+            remoteController.setArtworkConfiguration(100, 100);
+            remoteController.setSynchronizationMode(RemoteController.POSITION_SYNCHRONIZATION_CHECK);
+        }
+        catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            pw.flush();
+            Toast.makeText(this, sw.toString().substring(sw.toString().length() - 500), Toast.LENGTH_SHORT).show();
+        }
     }
+
+    // @Override
+    // public void onNotificationPosted(StatusBarNotification sbn) {
+    //     if (sbn.getPackageName() != null &&
+    //             sbn.getPackageName().contains("netease") && sbn.getPackageName().contains("music")) {
+
+    //         Notification notification = sbn.getNotification();
+    //         if (notification == null) return;
+
+    //         try {
+    //             RemoteViews rv = notification.contentView;
+    //             // Toast.makeText(this, rv == null ? "rv == null" : "rv != null", Toast.LENGTH_SHORT).show();
+    //             LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    //             Toast.makeText(this, "rv.getLayoutId(): " + rv.getLayoutId(), Toast.LENGTH_SHORT).show();
+    //             ViewGroup localView = (ViewGroup) inflater.inflate(rv.getLayoutId(), null);
+    //             Toast.makeText(this, "1: " + (localView == null ? "localView == null" : "localView != null"), Toast.LENGTH_SHORT).show();
+    //             // rv.reapply(getApplicationContext(), localView);
+    //             // Toast.makeText(this, "2: " + (localView == null ? "localView == null" : "localView != null"), Toast.LENGTH_SHORT).show();
+
+    //             Bundle extras = notification.extras;
+    //             // extra: hw_type = type_music
+    //             Toast.makeText(this, extras == null ? "extras == null" : "extras != null", Toast.LENGTH_SHORT).show();
+    //             String str = "";
+    //             for (String key : extras.keySet()) {
+    //                 str += key + ": " + extras.getString(key) + "\n";
+    //             }
+
+    //             Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+
+    //         } catch (Exception ex) {
+    //             StringWriter sw = new StringWriter();
+    //             PrintWriter pw = new PrintWriter(sw);
+    //             ex.printStackTrace(pw);
+    //             pw.flush();
+    //             Toast.makeText(this, sw.toString().substring(sw.toString().length() - 500), Toast.LENGTH_SHORT).show();
+    //         }
+    //     }
+    // }
+
+    // @Override
+    // public void onNotificationRemoved(StatusBarNotification sbn) {
+    //     if (sbn.getPackageName() != null &&
+    //             sbn.getPackageName().contains("netease") && sbn.getPackageName().contains("music")) {
+    //         Toast.makeText(this, "onNotificationRemoved", Toast.LENGTH_SHORT).show();
+    //     }
+    // }
 
     @Override
     public void onListenerConnected() {
@@ -22,12 +96,40 @@ public class MusicNotificationService extends NotificationListenerService {
     }
 
     @Override
-    public void onNotificationRemoved(StatusBarNotification sbn) {
-        Toast.makeText(this, "onNotificationRemoved", Toast.LENGTH_SHORT).show();
+    public void onListenerDisconnected() {
+        Toast.makeText(this, "onListenerDisconnected", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onListenerDisconnected() {
-        Toast.makeText(this, "onListenerDisconnected", Toast.LENGTH_SHORT).show();
+    public void onClientChange(boolean clearing) { }
+
+    @Override
+    public void onClientPlaybackStateUpdate(int state) { }
+
+    @Override
+    public void onClientPlaybackStateUpdate(int state, long stateChangeTimeMs, long currentPosMs, float speed) { }
+
+    @Override
+    public void onClientTransportControlUpdate(int transportControlFlags) { }
+
+    @Override
+
+    public void onClientMetadataUpdate(RemoteController.MetadataEditor metadataEditor) {
+        try {
+            String title = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_TITLE, "null");
+            String artist = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_ARTIST, "null");
+            String album = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_ALBUM, "null");
+            Long duration = metadataEditor.getLong(MediaMetadataRetriever.METADATA_KEY_DURATION, -1);
+            Toast.makeText(this, title + "\n" + artist + "\n" + album + "\n" + "\n" + duration, Toast.LENGTH_SHORT).show();
+            // Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            pw.flush();
+            Toast.makeText(this, sw.toString().substring(sw.toString().length() - 500), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
