b1ba55e72c46a6cac8816753320e084a3b2a2da9
AoiHosizora <a970335605@hotmail.com>
2019-09-14 21:36:51 +0800

    Add UI & Socket

--- NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MainActivity.java
+++ NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MainActivity.java
@@ -1,15 +1,24 @@
 package com.aoihosizora.neteasem2dserver;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.net.Socket;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         ServiceIntent = new Intent(this, MusicNotificationService.class);
 
-        PackageManager pm = getPackageManager();
-        pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-        pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+        // PackageManager pm = getPackageManager();
+        // pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
+        //         PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        // pm.setComponentEnabledSetting(new ComponentName(this, MusicNotificationService.class),
+        //         PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
 
         if (!isNotificationListenersEnabled()) {
             Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
@@ -63,26 +72,149 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.id_btn_server)
     Button m_btn_server;
 
+    @BindView(R.id.id_edt_ip)
+    TextView m_edt_ip;
+    @BindView(R.id.id_edt_port)
+    TextView m_edt_port;
+
+
     @OnClick(R.id.id_btn_server)
+    @UiThread
     void on_btn_server_clicked(View view) {
         if (m_btn_server.getText().equals(getString(R.string.btn_start))) {
-            m_btn_server.setText(getString(R.string.btn_stop));
-            try {
-                stopService(ServiceIntent);
-            }
-            catch (Exception ex) { }
-            startService(ServiceIntent);
-            Toast.makeText(this, "Start service success", Toast.LENGTH_SHORT).show();
-        } else {
-            m_btn_server.setText(getString(R.string.btn_start));
-            stopService(ServiceIntent);
-            Toast.makeText(this, "Stop service success", Toast.LENGTH_SHORT).show();
+            final String ip_str = m_edt_ip.getText().toString();
+            final String port_str = m_edt_port.getText().toString();
+
+            String ip_re = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+            String port_re = "([0-9]|[1-9]\\d{1,3}|[1-5]\\d{4}|6[0-4]\\d{4}|65[0-4]\\d{2}|655[0-2]\\d|6553[0-5])";
+
+            if (!ip_str.matches(ip_re))
+                showAlert(getString(R.string.alert_ip_format));
+            else if (!port_str.matches(port_re))
+                showAlert(getString(R.string.alert_port_format));
+            else
+                connect();
         }
+        else {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    disconnect();
+                }
+            }).start();
+        }
+    }
+
+    public static int REQUEST_NETWORK_PERMISSION_CODE = 1;
+
+    /**
+     * 格式正确，连接
+     */
+    @UiThread
+    private void connect() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.INTERNET
+            }, REQUEST_NETWORK_PERMISSION_CODE);
+        }
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(R.string.progress_linking));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ClientSendUtil.socket = new Socket(m_edt_ip.getText().toString(), Integer.parseInt(m_edt_port.getText().toString()));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, R.string.tst_connect_success, Toast.LENGTH_SHORT).show();
+                            m_btn_server.setText(getString(R.string.btn_stop));
+                            m_edt_ip.setEnabled(false);
+                            m_edt_port.setEnabled(false);
+
+                            try {
+                                stopService(ServiceIntent);
+                            } catch (Exception ex) { }
+                            startService(ServiceIntent);
+                        }
+                    });
+
+                } catch (Exception ex) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressDialog.dismiss();
+                            showAlert(getString(R.string.alert_connect_failed));
+                            m_btn_server.setText(getString(R.string.btn_start));
+                            m_edt_ip.setEnabled(true);
+                            m_edt_port.setEnabled(true);
+                        }
+                    });
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 断开连接
+     */
+    @UiThread
+    private void disconnect() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            m_btn_server.setText(getString(R.string.btn_start));
+                            stopService(ServiceIntent);
+                        }
+                    });
+                    ClientSendUtil.socket.close();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            m_btn_server.setText(getString(R.string.btn_start));
+                            m_edt_ip.setEnabled(true);
+                            m_edt_port.setEnabled(true);
+                        }
+                    });
+                }
+                catch (Exception ex) { }
+            }
+        }).start();
     }
 
     @Override
     protected void onDestroy() {
         stopService(ServiceIntent);
         super.onDestroy();
+    }
+
+    private void showAlert(String msg) {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.alert_title)
+                .setMessage(msg)
+                .setPositiveButton(R.string.alert_pos, null)
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_NETWORK_PERMISSION_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, R.string.tst_auth_failed, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MusicNotificationService.java
+++ NeteaseM2DServer/app/src/main/java/com/aoihosizora/neteasem2dserver/MusicNotificationService.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.MediaMetadataRetriever;
 import android.media.RemoteController;
 import android.service.notification.NotificationListenerService;
+import android.support.annotation.UiThread;
 import android.widget.Toast;
 
 import java.io.PrintWriter;
@@ -38,30 +39,48 @@ public class MusicNotificationService extends NotificationListenerService implem
     public void onClientPlaybackStateUpdate(int state) { }
 
     @Override
+    @UiThread
     public void onClientPlaybackStateUpdate(int state, long stateChangeTimeMs, long currentPosMs, float speed) {
         /*
         state: 3, stateChangeTimeMs: 2256786083, currentPosMs: 80807 <- Play
         state: 2, stateChangeTimeMs: 2256797365, currentPosMs: 85887 <- Stop
          */
-        String str = "state: " + state + "\ncurrentPosMs: " + currentPosMs / 1000.0;
+        final String str = "state: " + state + "\ncurrentPosMs: " + currentPosMs / 1000.0;
         Toast.makeText(this, "onClientPlaybackStateUpdate: \n" + str, Toast.LENGTH_SHORT).show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (ClientSendUtil.socket.isConnected())
+                    ClientSendUtil.sendMsg(str);
+            }
+        }).start();
     }
 
     @Override
+    @UiThread
     public void onClientMetadataUpdate(RemoteController.MetadataEditor metadataEditor) {
         try {
             String title = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_TITLE, "null");
             String artist = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_ARTIST, "null");
             String album = metadataEditor.getString(MediaMetadataRetriever.METADATA_KEY_ALBUM, "null");
             long duration = metadataEditor.getLong(MediaMetadataRetriever.METADATA_KEY_DURATION, -1) / 1000;
-            Toast.makeText(this, "title: " + title + "\nartist: " + artist + "\nalbum: " + album + "\nduration: " + duration, Toast.LENGTH_SHORT).show();
+
+            final String str = "title: " + title + "\nartist: " + artist + "\nalbum: " + album + "\nduration: " + duration;
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (ClientSendUtil.socket.isConnected())
+                        ClientSendUtil.sendMsg(str);
+                }
+            }).start();
         }
         catch (Exception ex) {
-
             Toast.makeText(this, getExceptionStr(ex), Toast.LENGTH_SHORT).show();
         }
     }
 
+    @UiThread
     private String getExceptionStr(Exception ex) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
